521f41f15edc34666ef12466ec21d769204cdc7f
Ryoichi Hara <rychhr@gmail.com>
2015-04-06 19:25:31 +0900

    Fix Thumbnail

--- YoutubePlayer/Models/Thumbnail.swift
+++ YoutubePlayer/Models/Thumbnail.swift
@@ -10,8 +10,39 @@ import Foundation
 import Realm
 
 class Thumbnail: RLMObject {
-    dynamic var resolution = ""  // default, medium, high, standard, maxres
+    dynamic var resolution = "default"  // default, medium, high, standard, maxres
     dynamic var url        = ""
     dynamic var width      = 0
     dynamic var height     = 0
+    
+    // MARK: - Initialization
+    
+    // Custom initializer is not supported yet (github.com/realm/realm-cocoa/issues/1101)
+    // init(resolution: String, json: JSON) {}
+    
+    class func modelFromJSON(json: JSON) -> Thumbnail {
+        return Thumbnail.modelFromJSON(json, resolution: nil)
+    }
+    
+    class func modelFromJSON(json: JSON, resolution: String?) -> Thumbnail {
+        let model = Thumbnail()
+        
+        if let resolution = resolution {
+            model.resolution = resolution
+        }
+        
+        if let url = json["url"].string {
+            model.url = url
+        }
+        
+        if let width = json["width"].int {
+            model.width = width
+        }
+        
+        if let height = json["height"].int {
+            model.height = height
+        }
+        
+        return model
+    }
 }
