f4adc4450b6ffa91fe3556e40643ae0986d1ea56
Ryoichi Hara <rychhr@gmail.com>
2015-04-07 12:17:02 +0900

    Update FeedViewModel

--- YoutubePlayer/ViewModels/FeedViewModel.swift
+++ YoutubePlayer/ViewModels/FeedViewModel.swift
@@ -11,5 +11,58 @@ import Bolts
 
 
 class FeedViewModel: NSObject {
-    private(set) var items = [VideoItem]()
+    private(set) var items = [FeedItemViewModel]()
+    private(set) var loading = false
+    private(set) var pagingEnabled = true
+    private var nextPageToken: String?
+    
+    override init() {
+        super.init()
+    }
+    
+    func fetchMostPopularVideos(#refresh: Bool) -> BFTask {
+        var fetchTask = BFTask(result: nil)
+        loading = true
+        
+        if refresh {
+            nextPageToken = nil
+        }
+        
+        fetchTask = fetchTask.continueWithBlock({ (task) -> AnyObject! in
+            return APIClient.fetchMostPopularVideos(pageToken: self.nextPageToken)
+        })
+        
+        fetchTask = fetchTask.continueWithSuccessBlock({ (task) -> AnyObject! in
+            if let dictionary = task.result as? [String: AnyObject] {
+                
+                if let videos = dictionary["videos"] as? [VideoItem] {
+                    let items = videos.map { (video: VideoItem) -> FeedItemViewModel in
+                        return FeedItemViewModel(item: video)
+                    }
+                    
+                    if refresh {
+                        self.items = items
+                    } else {
+                        self.items += items
+                    }
+                }
+                
+                if let nextPageToken = dictionary["nextPageToken"] as? String {
+                    self.nextPageToken = nextPageToken
+                } else {
+                    self.pagingEnabled = false
+                }                
+            }
+            
+            return task
+        })
+        
+        fetchTask = fetchTask.continueWithBlock({ (task) -> AnyObject! in
+            self.loading = false
+            
+            return task
+        })
+        
+        return fetchTask
+    }
 }
