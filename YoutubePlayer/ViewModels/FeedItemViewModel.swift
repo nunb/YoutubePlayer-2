42587d38458c8c38cf9d6a60ed100614f1d9f745
Ryoichi Hara <rychhr@gmail.com>
2015-04-07 11:15:12 +0900

    Add FeedItemViewModel

---
+++ YoutubePlayer/ViewModels/FeedItemViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItemViewModel.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/04/07.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+
+class FeedItemViewModel: NSObject {
+    
+    override init() {
+        super.init()
+    }
+}
