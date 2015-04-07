849d0ef18c0d538211d64f6a7c4eb5874e788c9b
Ryoichi Hara <rychhr@gmail.com>
2015-04-07 12:34:53 +0900

    Add FeedViewController

---
+++ YoutubePlayer/Views/Controllers/FeedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewController.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/04/07.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
