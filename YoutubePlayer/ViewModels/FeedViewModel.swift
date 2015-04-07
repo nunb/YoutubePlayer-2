d3e9b85af45f7e5bbde45bc559701533b1aea29c
Ryoichi Hara <rychhr@gmail.com>
2015-04-07 10:27:50 +0900

    Add FeedViewModel

---
+++ YoutubePlayer/ViewModels/FeedViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FeedViewModel.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/04/07.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+import Bolts
+
+
+class FeedViewModel: NSObject {
+    private(set) var items = [VideoItem]()
+}
