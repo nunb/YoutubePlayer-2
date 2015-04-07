4cc80173201ca801f1f716cd4c869acfe8f19609
Ryoichi Hara <rychhr@gmail.com>
2015-04-07 12:57:24 +0900

    Fix Podfile

---
+++ YoutubePlayer/Views/CellNodes/FeedVideoNode.swift
@@ -0,0 +1,13 @@
+//
+//  FeedVideoNode.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/04/07.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import Cocoa
+
+class FeedVideoNode: ASCellNode {
+
+}
