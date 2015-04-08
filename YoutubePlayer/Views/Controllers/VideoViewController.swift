56a30d9ce92e8042949b551c29a09fa0a1e130f5
Ryoichi Hara <rychhr@gmail.com>
2015-04-08 19:30:13 +0900

    Update VideoViewController

--- YoutubePlayer/Views/Controllers/VideoViewController.swift
+++ YoutubePlayer/Views/Controllers/VideoViewController.swift
@@ -11,11 +11,58 @@ import youtube_ios_player_helper
 
 class VideoViewController: UIViewController {
     
+    @IBOutlet weak var playerView: YTPlayerView!
+    @IBOutlet weak var playerMaskView: UIView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+
+    var viewModel: VideoViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        playerView.delegate = self
+        automaticallyAdjustsScrollViewInsets = false
+        
+        if let viewModel = viewModel {
+            
+            if let videoId = viewModel.videoId {
+                playerView.loadWithVideoId(videoId, playerVars: nil)
+                indicator.startAnimating()
+            }
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let viewModel = viewModel {
+            
+            if let videoId = viewModel.videoId {
+                playerView.loadWithVideoId(videoId, playerVars: nil)
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension VideoViewController: YTPlayerViewDelegate {
+    
+    func playerViewDidBecomeReady(playerView: YTPlayerView!) {
+        println("playerViewDidBecomeReady:")
+        
+        UIView.animateWithDuration(1.0,
+            animations: {
+                self.playerMaskView.alpha = 0.0
+            },
+            completion: { (finished) in
+                if finished {
+                    self.indicator.stopAnimating()
+                    self.playerMaskView.removeFromSuperview()
+                }
+            })
+        //playerView.playVideo()
     }
 }
