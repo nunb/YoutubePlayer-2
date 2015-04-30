eaab3c6c1d8d4513da9143e590b776fe40c6c43c
Ryoichi Hara <rychhr@gmail.com>
2015-04-30 13:01:01 +0900

    Fix Router and APIClient

--- YoutubePlayer/Utilities/Router.swift
+++ YoutubePlayer/Utilities/Router.swift
@@ -14,7 +14,7 @@ enum Router: URLRequestConvertible {
     
     // github.com/Alamofire/Alamofire#api-parameter-abstraction
     case MostPopular(pageToken: String?)
-    case Search(channelId: String?, pageToken: String?)
+    case Search(query: String?, pageToken: String?)
     
     // MARK: URLRequestConvertible
     
@@ -36,7 +36,7 @@ enum Router: URLRequestConvertible {
                 
                 return (.GET, "/videos", parameters)
             
-            case .Search(let channelId, let pageToken):
+            case .Search(let query, let pageToken):
                 var parameters: [String: AnyObject] = [
                     "key": Router.kGoogleAPIKey,
                     "part": "snippet",
@@ -44,8 +44,8 @@ enum Router: URLRequestConvertible {
                     "maxResults": 20,
                 ]
                 
-                if channelId != nil {
-                    parameters["channelId"] = channelId
+                if query != nil {
+                    parameters["q"] = query
                 }
                 
                 if pageToken != nil {
