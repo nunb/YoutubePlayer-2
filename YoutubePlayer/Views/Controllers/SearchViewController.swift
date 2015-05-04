0bf16e2c35a7d592faa829519536b7331fdc7701
Ryoichi Hara <rychhr@gmail.com>
2015-05-04 17:00:27 +0900

    Add SearchViewController

---
+++ YoutubePlayer/Views/Controllers/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/05/04.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
