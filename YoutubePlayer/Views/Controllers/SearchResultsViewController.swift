b12b7da68f0ea5599c6c418258bc95b12b03e9ff
Ryoichi Hara <rychhr@gmail.com>
2015-05-05 01:58:02 +0900

    Setup the search controller

--- YoutubePlayer/Views/Controllers/SearchResultsViewController.swift
+++ YoutubePlayer/Views/Controllers/SearchResultsViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    private let kCellId = "Cell"
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var results = [String]()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: kCellId)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+}
+
+extension SearchResultsViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kCellId, forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = results[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension SearchResultsViewController: UITableViewDelegate {
 }
--- YoutubePlayer/Views/Controllers/SearchViewController.swift
+++ YoutubePlayer/Views/Controllers/SearchViewController.swift
@@ -9,14 +9,56 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    private let kCellId = "Cell"
+    
+    // MARK: - Property
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var searchController: UISearchController?
+    
+    // TODO: Search History
+    var histories = [String]()
 
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: kCellId)
+        
+        let searchResultsVC = storyboard?
+            .instantiateViewControllerWithIdentifier("SearchResultsViewController")
+            as! SearchResultsViewController
+        
+        searchController = UISearchController(searchResultsController: searchResultsVC)
+        searchController!.hidesNavigationBarDuringPresentation = false
+        
+        navigationItem.titleView = searchController!.searchBar
+        
+        definesPresentationContext = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return histories.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kCellId, forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = histories[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension SearchResultsViewController: UITableViewDelegate {
 }
