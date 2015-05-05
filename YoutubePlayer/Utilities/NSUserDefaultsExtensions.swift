f73dcf9305b7049a1a8d2d53fc71424a4605829b
Ryoichi Hara <rychhr@gmail.com>
2015-05-05 23:33:37 +0900

    Add NSUserDefaultsExtensions

---
+++ YoutubePlayer/Utilities/NSUserDefaultsExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  NSUserDefaultsExtensions.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/05/05.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+
+extension NSUserDefaults {
+    
+}
