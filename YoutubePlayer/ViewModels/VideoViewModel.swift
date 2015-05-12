17c7566386fe0c6b081a5820fd323a2937ddf81f
Ryoichi Hara <rychhr@gmail.com>
2015-05-12 17:21:15 +0900

    Update VideoViewModel

--- YoutubePlayer/ViewModels/VideoViewModel.swift
+++ YoutubePlayer/ViewModels/VideoViewModel.swift
@@ -7,12 +7,15 @@
 //
 
 import Foundation
-
+import RealmSwift
 
 class VideoViewModel: NSObject {
     private(set) var videoId        : String?
     private(set) var title          : String?
     private(set) var itemDescription: String?
+    dynamic private(set) var bookmarked = false
+    
+    private var itemViewModel: FeedItemViewModel?
     
     init(itemViewModel: FeedItemViewModel) {
         super.init()
@@ -20,5 +23,37 @@ class VideoViewModel: NSObject {
         videoId = itemViewModel.itemId
         title   = itemViewModel.title
         itemDescription = itemViewModel.itemDescription
+        bookmarked = itemViewModel.bookmarked
+        
+        self.itemViewModel = itemViewModel
+    }
+    
+    func bookmark() {
+        let realm = Realm()
+        
+        if let videoId = videoId {
+            
+            if let user = realm.objectForPrimaryKey(User.self, key: "1") {
+                
+                if let videoItem = realm.objectForPrimaryKey(VideoItem.self, key: videoId) {
+                    realm.beginWrite()
+                    
+                    if let index = user.bookmarks.indexOf(videoItem) {
+                        user.bookmarks.removeAtIndex(index)
+                        bookmarked = false
+                    } else {
+                        user.bookmarks.append(videoItem)
+                        bookmarked = true
+                    }
+                    
+                    realm.commitWrite()
+                    
+                    // Update feed item property
+                    if let itemViewModel = itemViewModel {
+                        itemViewModel.bookmarked = bookmarked
+                    }
+                }
+            }
+        }
     }
 }
