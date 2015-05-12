9a0b3b90a62f3596df49502d4ba71d541fd7a330
Ryoichi Hara <rychhr@gmail.com>
2015-05-12 16:06:08 +0900

    Add User model

---
+++ YoutubePlayer/Models/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  YoutubePlayer
+//
+//  Created by Ryoichi Hara on 2015/05/12.
+//  Copyright (c) 2015年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class User: Object {
+    dynamic var userId = "1"
+    
+    // MARK: - Relation
+    
+    dynamic var bookmarks = List<VideoItem>()
+    
+    // MARK: - Override
+    
+    override static func primaryKey() -> String? {
+        return "userId"
+    }
+}
